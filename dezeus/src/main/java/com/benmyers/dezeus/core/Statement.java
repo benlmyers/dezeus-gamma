973b927cfa50e30b569457af5b3111d3777b1a17
Ben Myers <benl.myers01@gmail.com>
2022-05-15 23:55:29 -0700

    🔨 Statement utility methods

--- dezeus/src/main/java/com/benmyers/dezeus/core/Statement.java
+++ dezeus/src/main/java/com/benmyers/dezeus/core/Statement.java
@@ -1,8 +1,40 @@
 package com.benmyers.dezeus.core;
 
+import com.benmyers.dezeus.logic.Biconditional;
+import com.benmyers.dezeus.logic.Conditional;
+import com.benmyers.dezeus.logic.Conjunction;
+import com.benmyers.dezeus.logic.Disjunction;
+import com.benmyers.dezeus.logic.Negation;
+
 public abstract class Statement {
-    
+
     public abstract String toString();
+
     public abstract String toEnglish();
+
     public abstract String toLaTeX();
+
+    public Conjunction and(Statement b) {
+        return new Conjunction(this, b);
+    }
+
+    public Disjunction or(Statement b) {
+        return new Disjunction(this, b);
+    }
+
+    public Conditional implies(Statement cons) {
+        return new Conditional(this, cons);
+    }
+
+    public Conditional when(Statement ant) {
+        return new Conditional(ant, this);
+    }
+
+    public Biconditional iff(Statement b) {
+        return new Biconditional(this, b);
+    }
+
+    public Negation not(Statement s) {
+        return new Negation(s);
+    }
 }
--- dezeus/src/test/java/com/benmyers/dezeus/AppTest.java
+++ dezeus/src/test/java/com/benmyers/dezeus/AppTest.java
@@ -8,19 +8,17 @@ import com.benmyers.dezeus.core.Statement;
 
 import org.junit.Test;
 
-
-public class AppTest 
-{
+public class AppTest {
 
     @Test
-    public void checkNamings() {
+    public void checkNaming() {
         Statement p;
         try {
             p = new Atom("p");
             assertEquals("p", p.toString());
             assertEquals("p", p.toEnglish());
             assertEquals(" p ", p.toLaTeX());
-        } catch(Exception e) {
+        } catch (Exception e) {
             fail(e.getMessage());
         }
     }
